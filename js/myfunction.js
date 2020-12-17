3708ef73467abe15b895ce15ddf557dec2a038d0
Skeet WU <skeet.wu@nokia-sbell.com>
2020-12-17 15:17:01 +0800

    init with first version

---
+++ js/myfunction.js
@@ -0,0 +1,41 @@
+function getHeight(h) {
+	// alert($(window).height() - $('.ibox-title').outerHeight(true));
+	return $(window).height() - h;
+}
+function getWidth(w) {
+	// alert($(window).height() - $('.ibox-title').outerHeight(true));
+	return $(window).width() - w;
+}
+function copyToClipboard(txt) {
+    if (window.clipboardData) {
+        window.clipboardData.clearData();
+        window.clipboardData.setData("Text", txt);
+        alert("已经成功复制到剪帖板上！");
+    } else if (navigator.userAgent.indexOf("Opera") != -1) {
+        window.location = txt;
+		alert("---该浏览器不支持复制，请手动复制");
+    } else if (window.netscape) {
+        try {
+            netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+        } catch (e) {
+            alert("被浏览器拒绝！\n请在浏览器地址栏输入'about:config'并回车\n然后将'signed.applets.codebase_principal_support'设置为'true'");
+        }
+        var clip = Components.classes['@mozilla.org/widget/clipboard;1'].createInstance(Components.interfaces.nsIClipboard);
+        if (!clip) return;
+        var trans = Components.classes['@mozilla.org/widget/transferable;1'].createInstance(Components.interfaces.nsITransferable);
+        if (!trans) return;
+        trans.addDataFlavor('text/unicode');
+        var str = new Object();
+        var len = new Object();
+        var str = Components.classes["@mozilla.org/supports-string;1"].createInstance(Components.interfaces.nsISupportsString);
+        var copytext = txt;
+        str.data = copytext;
+        trans.setTransferData("text/unicode", str, copytext.length * 2);
+        var clipid = Components.interfaces.nsIClipboard;
+        if (!clip) return false;
+        clip.setData(trans, null, clipid.kGlobalClipboard);
+        alert("已经成功复制到剪帖板上！");
+    }else{		
+		console.log("该浏览器不支持复制，请手动复制");
+	}
+}
